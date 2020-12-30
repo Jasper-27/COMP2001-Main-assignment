9980fea15ab052612f7aec6bd97c1b65726d6f93
Jasper Cox <57488312+Jasper-27@users.noreply.github.com>
2020-12-30 00:05:15 +0000

    Started work on the main and data pages
    I bought in and tweaked the styling, as well as made some progress on the index page

---
+++ data.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Jasper's Linked data application</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
+    <link rel="stylesheet" href="myStyleSheet.css">
+    <link rel="stylesheet" href="w3.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+
+  <!-- Page Container -->
+  <div class="w3-content" >
+
+    <div align="center">
+      <h1>Jasper's linked data application </h1>
+    </div>
+
+    
+     
+  </div>
+
+
+
+  <script>
+   
+  </script>
+
+</body>
+</html>
