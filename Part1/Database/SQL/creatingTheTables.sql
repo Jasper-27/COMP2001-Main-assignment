bc6a042ceec7816d25eb72ee23c8945c00cf9d06
Jasper Cox <57488312+Jasper-27@users.noreply.github.com>
2021-01-07 15:31:47 +0000

    modified how the sessions table was created

--- Part1/Database/SQL/creatingTheTables.sql
+++ Part1/Database/SQL/creatingTheTables.sql
@@ -1,4 +1,4 @@
-/* CREATE TABLE dbo.Users (
+CREATE TABLE dbo.Users (
   user_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
   firstname VARCHAR(30) NOT NULL, 
   lastname VARCHAR(30) NOT NULL, 
@@ -6,18 +6,20 @@
   password VARCHAR(255) NOT NULL
   );
 
-  CREATE TABLE dbo.Passwords (
-      password_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
-      password VARCHAR(30) NOT NULL, 
-      date_changed DATETIME NOT NULL
-  ); 
+CREATE TABLE dbo.Passwords (
+    password_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    password VARCHAR(30) NOT NULL, 
+    date_changed DATETIME NOT NULL
+); 
 
-  CREATE TABLE dbo.Sessions (
-      Session_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
-      date_issued DATETIME NOT NULL 
-  ); 
+CREATE TABLE dbo.Sessions (
+    Session_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    user_id int FOREIGN KEY REFERENCES Users(user_id), 
+    date_issued DATETIME NOT NULL
+     
+); 
 
-  go  */
+  go 
 
 
   select * from TABLE Users where user_id = 1; 
