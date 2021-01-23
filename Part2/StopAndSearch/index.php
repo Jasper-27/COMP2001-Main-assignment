8830c7caf9f1f09caa9e5d4653943e614a475ada
Jasper Cox <57488312+Jasper-27@users.noreply.github.com>
2021-01-23 00:50:40 +0000

    now outputs pure JSON, with no added html formatting

--- Part2/StopAndSearch/index.php
+++ Part2/StopAndSearch/index.php
@@ -1,32 +1,18 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-  <title>data</title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-    
 <?php
-
-
+header("Content-Type: application/json"); 
 
 echo "{ \"@context\" : { \"Place\" : \"http://schema.org\", \"SaS\" : \"http://web.socem.plymouth.ac.uk\" }, \"Place\" : [ "; 
+$fname = "theData.csv"; 
 
+//Opening the file
+$f_pointer=fopen($fname,"r");
 
-
-
-  $fname = "theData.csv"; 
-
-  //Opening the file
-  $f_pointer=fopen($fname,"r");
-
-  //turning the csv into a nice array 
-  $header = null; 
+//turning the csv into a nice array 
+$header = null; 
   $line = 1; 
   
-  while(! feof($f_pointer)){
+while(! feof($f_pointer)){
     $ar=fgetcsv($f_pointer);
-
     
     if ($line == 1){
         $header = $ar; 
@@ -34,58 +20,32 @@ echo "{ \"@context\" : { \"Place\" : \"http://schema.org\", \"SaS\" : \"http://w
     }else{
 
         if ($line == 2){
-            echo "{"; echo "<br/>" ; 
+            echo "{"; 
            
         }else{
-            echo ", <br/> {"; echo "<br/>" ; 
+            echo ",{"; 
 
         }
 
-       
-
-        echo  "\"@type\": \"Place\","; echo "<br/>" ; 
-        echo "\"geo\" : { ";  echo "<br/>" ;
+        echo  "\"@type\": \"Place\",";
+        echo "\"geo\" : { "; 
             echo "\"@type\" : \"GeoCoordinates\", "; 
             echo "\"latitude\" : \"$ar[4]\", " ; 
             echo "\"longitude\" :  \"$ar[5]\" "; 
             echo "},";  
 
-        echo "\"$header[0] \" : \"$ar[0]\", "; echo "<br/>";
-        echo "\"SaS:$header[1] \" : \"$ar[1]\", "; echo "<br/>"; 
-        echo "\"SaS:$header[2] \" : \"$ar[2]\", "; echo "<br/>";
-        echo "\"SaS:$header[3] \" : \"$ar[3]\" "; echo "<br/>";
+        echo "\"$header[0] \" : \"$ar[0]\", ";
+        echo "\"SaS:$header[1] \" : \"$ar[1]\", "; 
+        echo "\"SaS:$header[2] \" : \"$ar[2]\", "; 
+        echo "\"SaS:$header[3] \" : \"$ar[3]\" "; 
         //echo "\"mdw:$header[4] \" : \"$ar[4], "; echo "<br/>";
 
         echo "}"; 
-       
-
-
     }
- 
     $line++; 
-
-    //outputting as json-ld
-
-    // echo "\"name\" : $ar[1]"
-
 }
 
 echo "]}"
 
 
 ?>
-
-</head>
-
-</html>
-
-
-
-<!-- "name" : "Millbay Park",
-                "mdw:area" : "0.17",
-                "mdw:Site_Type" : "Amenity"},{ "@type" : "Place",
-                "geo": {
-                    "@type" : "GeoCoordinates",
-                    "latitude" : 50.370332,
-                    "longitude" : -4.138688
-                 }, -->
